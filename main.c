7bd4811f94e038d803e0b2f8793415e2c43360c9
ImperialEQZ <alexanders.borchenko@gmail.com>
2024-05-20 13:22:28 +0300

    task_4/full_test

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include "matrix.h"
 #include "matrix.c"
+#include <string.h>
 
 void task_1(matrix *m, size_t count_query, size_t *array_query[]) {
     size_t row_1, col_1, row_2, col_2;
@@ -149,11 +150,95 @@ void test_task_3() {
     assert(areTwoMatricesEqual(&original_matrix, &transformed_matrix));
 }
 
+typedef struct domain {
+    size_t visits_site;
+    char name_site[1024];
+} domain;
+
+size_t find_domain_in_results(const domain *results, size_t size, char *s) {
+    for (size_t index = 0; index < size; index++) {
+        if (strcmp(results[index].name_site, s) == 0) {
+            return index;
+        }
+    }
+    return size;
+}
+
+bool find_number_in_arr(const size_t array[], size_t length, size_t num) {
+    for (size_t index = 0; index < length; index++) {
+        if (num == array[index])
+            return true;
+    }
+    return false;
+}
+void handlerDotPrtNotNull(domain *array, size_t index, char *dotPtr,
+                          domain results[], size_t *sizeResult) {
+    strcpy(array[index].name_site, dotPtr + 1);
+    size_t position = find_domain_in_results(results, *sizeResult, array[index].name_site);
+    if (position == *sizeResult) {
+        results[*sizeResult] = array[index];
+        *sizeResult += 1;
+    } else {
+        results[position].visits_site += array[index].visits_site;
+    }
+}
+
+void output_arrays_domainss(domain *results, size_t size) {
+    for (size_t index = 0; index < size; index++) {
+        printf("%zd %s\n", results[index].visits_site, results[index].name_site);
+    }
+}
+
+void task_4(domain arr[], size_t size) {
+    size_t close_index_sise[size];
+    size_t count_close = 0;
+    domain results[1024];
+    size_t size_res = 0;
+    for (size_t ind = 0; ind < size; ind++)
+        results[size_res++] = arr[ind];
+    while (count_close != size) {
+        for (size_t index = 0; index < size; index++) {
+            if (!find_number_in_arr(close_index_sise, count_close, index)) {
+                char *dot_ptr;//точка
+                dot_ptr = strchr(arr[index].name_site, '.');
+                if (dot_ptr != NULL)
+                    handlerDotPrtNotNull(arr, index, dot_ptr, results, &size_res);
+                else
+                    close_index_sise[count_close++] = index;
+            }
+        }
+    }
+    output_arrays_domainss(results, size_res);
+}
+//Пример из пособия №1
+void test_task_4_1() {
+    size_t size = 1;
+    domain arr[1] = {{9001, "discuss.codeforces.com"}};
+
+    task_4(arr, size);
+}
+//Пример из пособия №2
+void test_task_4_2() {
+    size_t size = 4;
+    domain arr[4] = {{900, "google.mail.com"},
+                       {50, "yahoo.com"},
+                       {1, "intel.mail.com"},
+                       {5, "wiki.org"}};
+    task_4(arr, size);
+}
+
+void test_task_4_all() {
+    test_task_4_1();
+    test_task_4_2();
+}
+
 void test_lab_20(){
     //test_task_1();
     //test_task_2();
-    test_task_3();
+    //test_task_3();
+    test_task_4_all();
 }
+
 int main() {
     test_lab_20();
 }
